d756b1e38e07f810ab3fb1169ac9b8c007b4c673
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-05-19 20:15:46 -0400

    added containers TopicPills and CourseEditor

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react'
 import CardDeck from './CardDeck'
-import ModuleList from './ModuleList'
-import LessonTabs from './LessonTabs'
+import CourseEditor from './CourseEditor'
+
 export default class CourseManager extends Component {
     render() {
         return (
             <div className="container-fluid">
                 <h1> Course Manager</h1>
-                <LessonTabs/>
-                <ModuleList/>
+                <CourseEditor/>
                 <CardDeck/>
             </div>
 
--- src/containers/LessonTabs.js
+++ src/containers/LessonTabs.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import TopicPills from './TopicPills'
+
 
 export default class LessonTabs
     extends React.Component {
-    render() { return(
-        <ul class="nav nav-tabs">
-            <li class="nav-item"><a class="nav-link active"
-                                    href="#">Active Tab</a></li>
-            <li class="nav-item"><a class="nav-link"
-                                    href="#">Another Tab</a></li>
-        </ul>
-    );}}
+    render() {
+        return (
+            <div>
+                <ul class="nav nav-tabs">
+                    <li class="nav-item"><a class="nav-link active"
+                                            href="#">Active Tab</a></li>
+                    <li class="nav-item"><a class="nav-link"
+                                            href="#">Another Tab</a></li>
+                </ul>
+                <br/>
+                <TopicPills/>
+            </div>
+
+        );
+    }
+}
