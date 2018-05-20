a9349fffcf38bde6fbec468ff42c920997524dc3
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-05-20 15:34:41 -0400

    added CourseList with CourseRows

---
+++ src/containers/CourseList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import CourseRow from '../components/CourseRow'
+class CourseList extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div>
+            <h2>Course List</h2>
+                <table>
+                    <thead><tr><th>Title</th></tr></thead>
+                    <tbody>
+                    <CourseRow/>
+                    <CourseRow/>
+                    <CourseRow/>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+export default CourseList;
--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import CardDeck from './CardDeck'
 import CourseEditor from './CourseEditor'
+import CourseList from './CourseList'
 
 export default class CourseManager extends Component {
     render() {
         return (
             <div className="container-fluid">
                 <h1> Course Manager</h1>
+                <CourseList/>
                 <CourseEditor/>
                 <CardDeck/>
             </div>
