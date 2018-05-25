ebea28ca86fbd209853520f479dbe1232a4a349f
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-05-25 13:22:15 -0400

    fixed activeModule button visibility

--- src/components/ModuleListItem.js
+++ src/components/ModuleListItem.js
@@ -58,13 +58,14 @@ export default class ModuleListItem
                         {this.props.module.title}
                     </Link>
                     <span className="float-right">
-                        <i className="btn fa fa-2x fa-times" onClick={() => {
+                        <button className={(this.props.active == this.props.module.id) ? 'btn btn-dark fa fa-2x fa-square invisible' : "btn btn-dark fa fa-2x fa-times"}
+                           onClick={() => {
                             (this.props.active != this.props.module.id)
                             ?
                             this.openModal()
                             //this.props.delete(this.props.courseId, this.props.module.id)
                             : null}}>
-                        </i>
+                        </button>
                     </span>
                     <div>
                         <Modal
